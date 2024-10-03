4a043f9d90113db68246e7426a3718fbeb0e40b6
Fi <158217226+lightfi7@users.noreply.github.com>
2024-10-02 17:01:35 -0700

    Update Config.java

--- src/config/Config.java
+++ src/config/Config.java
@@ -1,5 +1,16 @@
 package config;
 
 public class Config {
+    /* MODEL*/
+
+    /* SOCKET
+    * */
+    public static String HOST = "localhost";
+    public static int PORT = 8777;
+    public static String ENDPOINT = "ws:localhost:8778";
+
+    /* LOGGER
+    * */
+    public static String LOG_PATH = "";
 }
 
