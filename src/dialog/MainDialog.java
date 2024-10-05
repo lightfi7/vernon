af599f6691a3c3b9480f26ef3528537ad6c99148
Fi <158217226+lightfi7@users.noreply.github.com>
2024-10-04 17:09:46 -0700

    update code.

--- src/dialog/MainDialog.java
+++ src/dialog/MainDialog.java
@@ -21,6 +21,77 @@ public class MainDialog extends JDialog {
         logListModel = new DefaultListModel<>();
         logList = new JList<>(logListModel);
 
+        // Create a menu
+        JMenuBar menuBar = new JMenuBar();
+        JMenu modelMenu = new JMenu("Model");
+        JMenu trainMenu = new JMenu("Train");
+
+        JMenuItem m2Item = new JMenuItem("M2");
+        m2Item.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Application.instance.classifier.change(2);
+            }
+        });
+
+        JMenuItem m7Item = new JMenuItem("M7");
+        m7Item.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Application.instance.classifier.change(7);
+            }
+        });
+
+        JMenuItem m15Item = new JMenuItem("M15");
+        m15Item.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Application.instance.classifier.change(15);
+            }
+
+        });
+
+        modelMenu.add(m2Item);
+        modelMenu.add(m7Item);
+        modelMenu.add(m15Item);
+
+        JMenuItem t2Item = new JMenuItem("M2");
+        t2Item.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Application.instance.classifier.train(2);
+            }
+        });
+
+        JMenuItem t7Item = new JMenuItem("M7");
+        t7Item.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Application.instance.classifier.train(7);
+            }
+        });
+
+        JMenuItem t15Item = new JMenuItem("M15");
+        t15Item.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Application.instance.classifier.train(15);
+            }
+        });
+
+        trainMenu.add(t2Item);
+        trainMenu.add(t7Item);
+        trainMenu.add(t15Item);
+
+        menuBar.add(modelMenu);
+        menuBar.add(trainMenu);
+
         // Create a scroll pane for the log list
         JScrollPane scrollPane = new JScrollPane(logList);
 
@@ -40,6 +111,7 @@ public class MainDialog extends JDialog {
         });
 
         // Add components to the dialog
+        add(menuBar, BorderLayout.NORTH);
         add(scrollPane, BorderLayout.CENTER);
         add(startButton, BorderLayout.SOUTH);
 
@@ -53,8 +125,8 @@ public class MainDialog extends JDialog {
     // Method to add log messages to the list
     public void addLogMessage(String message) {
         int count = logListModel.getSize();
-        if(count>300){
-            logListModel.removeRange(0, count-300);
+        if (count > 300) {
+            logListModel.removeRange(0, count - 300);
         }
         logListModel.addElement(message);
     }
--- src/model/J48Classifier.java
+++ src/model/J48Classifier.java
@@ -10,7 +10,7 @@ import weka.core.converters.CSVSaver;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.classifiers.trees.J48;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -21,31 +21,69 @@ import weka.filters.unsupervised.attribute.Remove;
 
 public class J48Classifier implements EventHandler {
 
-    public static J48 tree = new J48();
+    public static J48 tree = null;
     public static ArrayList<Attribute> attributes = new ArrayList<>();
     public int lastPosition = -1;
-    public int lastOutput = -1;
+    public int M = Config.M;
     public Double lastEquity = 0.0;
 
     public J48Classifier() {
 
         try {
             Logger.log("Initializing J48 Classifier...");
+
             // Load the ARFF file
             DataSource source = new DataSource(Config.ARFF_FILE);
             Instances data = source.getDataSet();
 
             // Set the attributes from data instance
-
             for (int i = 0; i < data.numAttributes(); i++) {
                 attributes.add(new Attribute(data.attribute(i).name()));
             }
 
+            load(Config.M);
+            //train(Config.M);
+
+            Logger.log("Initialized J48 Classifier");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Logger.log("Error: " + e.getMessage());
+        }
+
+    }
+
+    public void load(int M) throws Exception {
+        if (this.M == M) return;
+        Logger.log("Loading M" + M + "model...");
+        Application.instance.isReady = false;
+        // deserialize model
+        ObjectInputStream ois = new ObjectInputStream(
+                new FileInputStream("data/j48_" + M + ".model"));
+        tree = (J48) ois.readObject();
+        ois.close();
+        this.M = M;
+        Logger.log("Loaded M" + M + "model");
+        Application.instance.isReady = true;
+    }
+
+    public void train(int M) {
+
+        try {
+            Logger.log("Training M" + M + "model...");
+            tree = new J48();
+            // Load the ARFF file
+            DataSource source = new DataSource(Config.ARFF_FILE);
+            Instances data = source.getDataSet();
+
+            // Set the attributes from data instance
+            for (int i = 0; i < data.numAttributes(); i++) {
+                attributes.add(new Attribute(data.attribute(i).name()));
+            }
             // Set the class index (assumed to be the last column)
             if (data.classIndex() == -1) {
                 data.setClassIndex(data.numAttributes() - 1);
             }
-
             // Remove specific columns (1st, 2nd, 3rd, 5th, 14th, 38th, 39th, 49th)
             Remove remove = new Remove();
             final String[] removeOptions = new String[]{"-R", "1,2,3,5,14,38,39,49"};
@@ -56,13 +94,13 @@ public class J48Classifier implements EventHandler {
             Instances filteredData = Filter.useFilter(data, remove);
 
             // Train the J48 model with specified parameters (-c 0.25 -m 2)
-            if(Config.M == 2){
+            if (M == 2) {
                 String[] options = new String[]{"-C", "0.25", "-M", "2"};
                 tree.setOptions(options);
-            }else if(Config.M == 7){
+            } else if (M == 7) {
                 String[] options = new String[]{"-C", "0.25", "-M", "7"};
                 tree.setOptions(options);
-            }else if(Config.M == 15){
+            } else if (M == 15) {
                 String[] options = new String[]{"-C", "0.25", "-M", "15"};
                 tree.setOptions(options);
             }
@@ -70,14 +108,30 @@ public class J48Classifier implements EventHandler {
             // Now build the classifier on the entire dataset
             tree.buildClassifier(filteredData);
 
-            Application.instance.isReady = true;
-            Logger.log("Initialized J48 Classifier");
-
+            // serialize model
+            ObjectOutputStream oos = new ObjectOutputStream(
+                    new FileOutputStream("data/j48_" + M + ".model"));
+            oos.writeObject(tree);
+            oos.flush();
+            oos.close();
+            Logger.log("Trained M" + M + "model");
         } catch (Exception e) {
             e.printStackTrace();
             Logger.log("Error: " + e.getMessage());
         }
+    }
 
+    public void change(int M) {
+        try {
+            Logger.log("Changing to M" + M + "model...");
+            Application.instance.isReady = false;
+            load(M);
+            Logger.log("Changed to M" + M + "model");
+            Application.instance.isReady = true;
+        } catch (Exception e) {
+            Logger.log("Error: " + e.getMessage());
+
+        }
     }
 
     public void predict(double[] features, String time, Double epoch, Double equityLast) {
@@ -120,17 +174,16 @@ public class J48Classifier implements EventHandler {
 
             Logger.log("Outbound: " + prediction);
 
-            int ep = epoch.intValue()/10000;
-            ep*=10;
+            int ep = epoch.intValue() / 10000;
+            ep *= 10;
 
             if (lastPosition == 0) {
-                if (prediction == 1)
-                {
+                if (prediction == 1) {
                     lastPosition = 1;
                     Logger.log(String.format("COVER %d SPY COVER %s %s", ep, lastEquity.toString(), equityLast.toString()));
-                    Logger.log(String.format("BUY %d SPY BUY %s %s", ep+1, lastEquity.toString(), equityLast.toString()));
+                    Logger.log(String.format("BUY %d SPY BUY %s %s", ep + 1, lastEquity.toString(), equityLast.toString()));
                     Application.instance.client.send(String.format("COVER %d SPY COVER %s %s", ep, lastEquity.toString(), equityLast.toString()));
-                    Application.instance.client.send(String.format("BUY %d SPY BUY %s %s", ep+1, lastEquity.toString(), equityLast-10.0));
+                    Application.instance.client.send(String.format("BUY %d SPY BUY %s %s", ep + 1, lastEquity.toString(), equityLast - 10.0));
                     lastEquity = equityLast;
                 }
             } else if (lastPosition == 1) {
@@ -139,7 +192,7 @@ public class J48Classifier implements EventHandler {
                     Logger.log(String.format("SELL %d SPY SELL %s %s", ep, lastEquity.toString(), equityLast.toString()));
                     Logger.log(String.format("SHORT %d SPY SHORT %s %s", ep + 1, lastEquity.toString(), equityLast.toString()));
                     Application.instance.client.send(String.format("SELL %d SPY SELL %s %s", ep, lastEquity.toString(), equityLast.toString()));
-                    Application.instance.client.send(String.format("SHORT %d SPY SHORT %s %s", ep + 1, equityLast.toString(), lastEquity+10.0));
+                    Application.instance.client.send(String.format("SHORT %d SPY SHORT %s %s", ep + 1, equityLast.toString(), lastEquity + 10.0));
                     lastEquity = equityLast;
                 }
             }
@@ -158,9 +211,9 @@ public class J48Classifier implements EventHandler {
     @Override
     public void onDataReceived(String jsonString) {
         try {
-            Logger.log("Inbound data: "+jsonString);
+            Logger.log("Inbound data: " + jsonString);
 
-            if(!Application.instance.isReady){
+            if (!Application.instance.isReady) {
                 Logger.log("Application isn't ready");
                 return;
             }
@@ -186,10 +239,10 @@ public class J48Classifier implements EventHandler {
                 values[i] = features.get(i);
             }
 
-            predict(values,jsonObject.getString("time"), jsonObject.getDouble("epoch"),jsonObject.getDouble("equityLast"));
+            predict(values, jsonObject.getString("time"), jsonObject.getDouble("epoch"), jsonObject.getDouble("equityLast"));
 
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             Logger.log("Error: " + e.getMessage());
         }
