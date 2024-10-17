fa8ae67bf89ee976839afe65581a02b31e600841
Fi <158217226+lightfi7@users.noreply.github.com>
2024-10-17 02:31:01 -0700

    fix j48 and optimizing the code

--- src/model/J48Classifier.java
+++ src/model/J48Classifier.java
@@ -1,124 +1,121 @@
 package model;
+
 import app.Application;
 import config.Config;
 import utils.EventHandler;
 import utils.Logger;
+import weka.classifiers.Evaluation;
 import weka.core.Attribute;
 import weka.core.DenseInstance;
 import weka.core.Instances;
-import weka.core.converters.CSVLoader;
-import weka.core.converters.CSVSaver;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.classifiers.trees.J48;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.json.JSONObject;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.Remove;
 
+import org.json.JSONObject;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class J48Classifier implements EventHandler {
 
-    public static J48 tree = null;
-    public static ArrayList<Attribute> attributes = new ArrayList<>();
-    public int lastPosition = 0;
-    public int M = 0;
-    public Double lastEquity = 0.0;
+    private static J48 tree = null;
+    private static Evaluation eval = null;
+    private int lastPosition = 0;
+    private int M = 0;
+    private Double lastEquity = 0.0;
+
+    private static final String MODEL_PATH_TEMPLATE = "data/j48_%d.model";
+    private static final String[] REMOVE_OPTIONS = {"-R","1,2,3,5,13,38,39,49"};
 
     public J48Classifier() {
-
         try {
             Logger.log("Initializing J48 Classifier...");
-
-            // Load the ARFF file
-            DataSource source = new DataSource(Config.ARFF_FILE);
-            Instances data = source.getDataSet();
-
-            // Set the attributes from data instance
-            for (int i = 0; i < data.numAttributes(); i++) {
-                attributes.add(new Attribute(data.attribute(i).name()));
-            }
-
-            this.load(Config.M);
-            //train(Config.M);
-
+            loadModel(Config.M);
             Logger.log("Initialized J48 Classifier");
-
         } catch (Exception e) {
-            e.printStackTrace();
-            Logger.log("Error: " + e.getMessage());
+            logError(e);
         }
-
     }
 
-    public void load(int M) throws Exception {
+    private void loadModel(int M) throws Exception {
         if (this.M == M) return;
+
         Logger.log("Loading M" + M + " model...");
         Application.instance.setIsReady(false);
-        // deserialize model
-        ObjectInputStream ois = new ObjectInputStream(
-                new FileInputStream("data/j48_" + M + ".model"));
-        tree = (J48) ois.readObject();
-        ois.close();
+        tree = deserializeModel(M);
+
+        Instances data = loadAndFilterData();
+        eval = new Evaluation(data);
+        eval.crossValidateModel(tree, data, 10, new Random(1));
+        printCrossValidationResults();
+
         this.M = M;
         Logger.log("Loaded M" + M + " model");
         Application.instance.setIsReady(true);
     }
 
-    public void train(int M) {
+    private J48 deserializeModel(int M) throws IOException, ClassNotFoundException {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(String.format(MODEL_PATH_TEMPLATE, M)))) {
+            return (J48) ois.readObject();
+        }
+    }
 
+    private Instances loadAndFilterData() throws Exception {
+        DataSource source = new DataSource(Config.ARFF_FILE);
+        Instances data = source.getDataSet();
+
+        if (data.classIndex() == -1) {
+            data.setClassIndex(data.numAttributes() - 1);
+        }
+
+        Remove remove = new Remove();
+        remove.setOptions(REMOVE_OPTIONS);
+        remove.setInputFormat(data);
+        return Filter.useFilter(data, remove);
+    }
+
+    private void printCrossValidationResults() {
+        System.out.println(eval.toSummaryString("\n10-Fold Cross-Validation Results\n======\n", false));
+    }
+
+    public void train(int M) {
         try {
             Logger.log("Training M" + M + " model...");
             tree = new J48();
-            // Load the ARFF file
-            DataSource source = new DataSource(Config.ARFF_FILE);
-            Instances data = source.getDataSet();
-
-            // Set the attributes from data instance
-            for (int i = 0; i < data.numAttributes(); i++) {
-                attributes.add(new Attribute(data.attribute(i).name()));
-            }
-            // Set the class index (assumed to be the last column)
-            if (data.classIndex() == -1) {
-                data.setClassIndex(data.numAttributes() - 1);
-            }
-            // Remove specific columns (1st, 2nd, 3rd, 5th, 13th, 38th, 39th, 49th)
-            Remove remove = new Remove();
-            final String[] removeOptions = new String[]{"-R", "1,2,3,5,13,38,39,49"};
-            remove.setOptions(removeOptions);
-
-            remove.setInputFormat(data);
-
-            Instances filteredData = Filter.useFilter(data, remove);
-
-            // Train the J48 model with specified parameters (-c 0.25 -m 2)
-            if (M == 2) {
-                String[] options = new String[]{"-C", "0.25", "-M", "2"};
-                tree.setOptions(options);
-            } else if (M == 7) {
-                String[] options = new String[]{"-C", "0.25", "-M", "7"};
-                tree.setOptions(options);
-            } else if (M == 15) {
-                String[] options = new String[]{"-C", "0.25", "-M", "15"};
-                tree.setOptions(options);
-            }
-
-            // Now build the classifier on the entire dataset
-            tree.buildClassifier(filteredData);
-
-            // serialize model
-            ObjectOutputStream oos = new ObjectOutputStream(
-                    new FileOutputStream("data/j48_" + M + ".model"));
-            oos.writeObject(tree);
-            oos.flush();
-            oos.close();
+            Instances data = loadAndFilterData();
+            setJ48Options(M);
+            tree.buildClassifier(data);
+            serializeModel(M);
             Logger.log("Trained M" + M + " model");
         } catch (Exception e) {
-            e.printStackTrace();
-            Logger.log("Error: " + e.getMessage());
+            logError(e);
+        }
+    }
+
+    private void setJ48Options(int M) throws Exception {
+        String[] options;
+        switch (M) {
+            case 2:
+                options = new String[]{"-C", "0.25", "-M", "2"};
+                break;
+            case 7:
+                options = new String[]{"-C", "0.25", "-M", "7"};
+                break;
+            case 15:
+                options = new String[]{"-C", "0.25", "-M", "15"};
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid M value: " + M);
+        }
+        tree.setOptions(options);
+    }
+
+    private void serializeModel(int M) throws IOException {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(String.format(MODEL_PATH_TEMPLATE, M)))) {
+            oos.writeObject(tree);
         }
     }
 
@@ -126,84 +123,55 @@ public class J48Classifier implements EventHandler {
         try {
             Logger.log("Changing to M" + M + " model...");
             Application.instance.setIsReady(false);
-            load(M);
+            loadModel(M);
             Logger.log("Changed to M" + M + " model");
             Application.instance.setIsReady(true);
         } catch (Exception e) {
-            Logger.log("Error: " + e.getMessage());
-
+            logError(e);
         }
     }
 
     public void predict(double[] features, String time, Double epoch, Double equityLast) {
         try {
-            Instances data = new Instances("InboundInstance", attributes, 0);
-
+            Instances data = new Instances("InboundInstance", createAttributes(), 0);
             data.add(new DenseInstance(1.0, features));
 
             if (data.classIndex() == -1) {
                 data.setClassIndex(data.numAttributes() - 1);
             }
 
-            // Remove specific columns (1st, 2nd, 3rd, 5th, 14th, 38th, 39th, 49th)
             Remove remove = new Remove();
-            final String[] removeOptions = new String[]{"-R", "1,2,3,5,14,38,39,49"};
-            remove.setOptions(removeOptions);
-
+            remove.setOptions(new String[]{"-R", "1,2,3,5,14,38,39,49"});
             remove.setInputFormat(data);
-
             Instances filteredData = Filter.useFilter(data, remove);
 
-            // Create a new dataset with predicted labels
-            Instances outputData = new Instances(data);
-            outputData.insertAttributeAt(new weka.core.Attribute("PredictedClass"), outputData.numAttributes());
-
-
-            // Classify each instance and add the prediction to the outputData
-            /*
-            double[] predictions = new double[outputData.numInstances()];
-            for (int i = 0; i < filteredData.numInstances(); i++) {
-                double prediction = tree.classifyInstance(filteredData.instance(i));
-                predictions[i] = prediction;
-                System.out.println(prediction);
-                outputData.instance(i).setValue(outputData.numAttributes() - 1, prediction);
-            }*/
-
             double prediction = tree.classifyInstance(filteredData.instance(0));
-            System.out.println(prediction);
-            outputData.instance(0).setValue(outputData.numAttributes() - 1, prediction);
-
-            Logger.log("Outbound: " + prediction);
-
-            int ep = epoch.intValue() / 10000;
-            ep *= 10;
-
-            if (lastPosition == 0) {
-                if (prediction == 1) {
-                    lastPosition = 1;
-                    Logger.log2(String.format("COVER %d SPY COVER %s %s", ep, lastEquity.toString(), equityLast.toString()));
-                    Logger.log2(String.format("BUY %d SPY BUY %s %s", ep + 1, lastEquity.toString(), equityLast.toString()));
-                    Application.instance.client.send(String.format("COVER %d SPY COVER %s %s", ep, lastEquity.toString(), equityLast.toString()));
-                    Application.instance.client.send(String.format("BUY %d SPY BUY %s %s", ep + 1, lastEquity.toString(), equityLast - 10.0));
-                    lastEquity = equityLast;
-                }
-            } else if (lastPosition == 1) {
-                if (prediction == 0) {
-                    lastPosition = 0;
-                    Logger.log2(String.format("SELL %d SPY SELL %s %s", ep, lastEquity.toString(), equityLast.toString()));
-                    Logger.log2(String.format("SHORT %d SPY SHORT %s %s", ep + 1, lastEquity.toString(), equityLast.toString()));
-                    Application.instance.client.send(String.format("SELL %d SPY SELL %s %s", ep, lastEquity.toString(), equityLast.toString()));
-                    Application.instance.client.send(String.format("SHORT %d SPY SHORT %s %s", ep + 1, equityLast.toString(), lastEquity + 10.0));
-                    lastEquity = equityLast;
-                }
-            }
-
-            // Save the output with predictions to a new CSV file
-
-
+            handlePrediction(prediction, epoch, equityLast);
         } catch (Exception e) {
-            Logger.log("Error: " + e.getMessage());
+            logError(e);
         }
+    }
+
+    private ArrayList<Attribute> createAttributes() {
+        return new ArrayList<>(Arrays.asList(/* Define attributes here */));
+    }
+
+    private void handlePrediction(double prediction, Double epoch, Double equityLast) {
+        int ep = epoch.intValue() / 10000 * 10;
+
+        if (lastPosition == 0 && prediction == 1) {
+            lastPosition = 1;
+            logAndSendTradeActions("COVER", ep, equityLast);
+        } else if (lastPosition == 1 && prediction == 0) {
+            lastPosition = 0;
+            logAndSendTradeActions("SELL", ep, equityLast);
+        }
+        lastEquity = equityLast;
+    }
+
+    private void logAndSendTradeActions(String action, int ep, Double equityLast) {
+        Logger.log2(String.format("%s %d SPY %s", action, ep, equityLast));
+        Application.instance.client.send(String.format("%s %d SPY %s", action, ep, equityLast));
     }
 
     @Override
@@ -216,11 +184,8 @@ public class J48Classifier implements EventHandler {
                 return;
             }
 
-            // Parse JSON string to a JSONObject
             JSONObject jsonObject = new JSONObject(jsonString);
-
-            // Prepare the data as a Map
-            Map<String, Object> jsonData = new HashMap<String, Object>();
+            Map<String, Object> jsonData = new HashMap<>();
             jsonData.put("time", jsonObject.getString("time"));
             jsonData.put("epoch", jsonObject.getDouble("epoch"));
             jsonData.put("equityLast", jsonObject.getDouble("equityLast"));
@@ -230,75 +195,16 @@ public class J48Classifier implements EventHandler {
             jsonData.put("TTTT", jsonObject.getDouble("TTTT"));
 
             ArrayList<Double> features = Feature.parse(jsonData);
-            System.out.println(features);
-
-            double[] values = new double[features.size()];
-            for (int i = 0; i < features.size(); i++) {
-                values[i] = features.get(i);
-            }
-
+            double[] values = features.stream().mapToDouble(Double::doubleValue).toArray();
             predict(values, jsonObject.getString("time"), jsonObject.getDouble("epoch"), jsonObject.getDouble("equityLast"));
 
-
         } catch (Exception e) {
-            e.printStackTrace();
-            Logger.log("Error: " + e.getMessage());
+            logError(e);
         }
     }
 
-/*
-    public static void main(String[] args) throws Exception {
-        // Load the ARFF file
-        DataSource source = new DataSource("data/vernon.arff");
-        Instances data = source.getDataSet();
-
-        // Set the class index (assumed to be the last column)
-        if (data.classIndex() == -1) {
-            data.setClassIndex(data.numAttributes() - 1);
-        }
-
-        System.out.println(data.get(1).numValues());
-
-        // Remove specific columns (1st, 2nd, 3rd, 5th, 38th, 39th, 49th)
-        Remove remove = new Remove();
-        final String[] removeOptions = new String[]{"-R", "1,2,3,5,38,39,49"};
-        remove.setOptions(removeOptions);
-
-        remove.setInputFormat(data);
-
-        Instances filteredData = Filter.useFilter(data, remove);
-
-        // Train the J48 model with specified parameters (-c 0.25 -m 2)
-        String[] options = new String[]{"-C", "0.25", "-M", "2"};
-        tree.setOptions(options);
-
-        // Perform 10-fold cross-validation for evaluation
-        Evaluation eval = new Evaluation(filteredData);
-        eval.crossValidateModel(tree, filteredData, 10, new Random(1));
-
-        // Print cross-validation results
-        System.out.println(eval.toSummaryString("\n10-Fold Cross-Validation Results\n======\n", false));
-
-        // Now build the classifier on the entire dataset
-        tree.buildClassifier(filteredData);
-
-        // Create a new dataset with predicted labels
-        Instances outputData = new Instances(data);
-        outputData.insertAttributeAt(new weka.core.Attribute("PredictedClass"), outputData.numAttributes());
-
-        // Classify each instance and add the prediction to the outputData
-        for (int i = 0; i < filteredData.numInstances(); i++) {
-            double prediction = tree.classifyInstance(filteredData.instance(i));
-            outputData.instance(i).setValue(outputData.numAttributes() - 1, prediction);
-        }
-
-        // Save the output with predictions to a new CSV file
-        CSVSaver saver = new CSVSaver();
-        saver.setInstances(outputData);
-        saver.setFile(new File("output.csv"));
-        saver.writeBatch();
-
-        System.out.println("Final model built on the entire dataset and output saved to CSV.");
+    private void logError(Exception e) {
+        e.printStackTrace();
+        Logger.log("Error: " + e.getMessage());
     }
-*/
 }
